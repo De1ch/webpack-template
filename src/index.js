f57bc2eb0c646a69623745cda6dac899bb687a43
Nikita Deich <54121126+De1ch@users.noreply.github.com>
2020-06-10 03:25:28 +0600

    configure Webpack to work with css / sass, install / configure plugins

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-import './js/common.js'
+import './js/script.js'
+import './css/main.css'
+import './sass/main.sass'
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = {
     entry: {
         main: './src/index.js',
@@ -15,9 +15,43 @@ module.exports = {
             test: /\.js$/,
             loader: 'babel-loader',
             exclude: '/node_modules/'
+        },{
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: { sourceMap: true }
+                }, {
+                    loader: 'postcss-loader',
+                    options: { sourceMap: true, config: { path: 'postcss.config.js' } }
+                }
+            ]
+        },{
+            test: /\.sass$/,
+            use: [
+                'style-loader',
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: { sourceMap: true }
+                }, {
+                    loader: 'postcss-loader',
+                    options: { sourceMap: true, config: { path: 'postcss.config.js' } }
+                }, {
+                    loader: 'sass-loader',
+                    options: { sourceMap: true }
+                }
+            ]
         }]
     },
     devServer: {
       overlay: true
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+        })
+    ],
 }
